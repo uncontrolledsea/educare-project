170dcffdfe4cf3c6ca748b69035ac6345a24365d
shubham <smore4569@gmail.com>
2026-04-29 01:56:22 +0530

    fix: updated back to main page button

--- frontend/api.js
+++ frontend/api.js
@@ -1,1 +1,7 @@
 const API = process.env.REACT_APP_API_URL;
+
+
+
+
+
+// ok now project is working but when we play any of the game and after that we want to go to main page where all games listed so please provide the button in each game to go to main game page 
